4964ab28a3a2b46bea69678feb0c7cac03d273b9
viet247 <viet24799@gmail.com>
2026-05-29 11:14:56 +0700

    feat: transfer point

---
+++ Services/ITransactionService.cs
@@ -0,0 +1,7 @@
+﻿namespace FPTRewardSystem.API.Services
+{
+    public interface ITransactionService
+    {
+        Task<bool> TransferPointAsync(Guid fromWalletID, Guid toWalletID, decimal amount, string description);
+    }
+}

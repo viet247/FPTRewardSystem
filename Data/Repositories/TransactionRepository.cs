c36f2e6f7e0a70e6817a98aa997c3b695c430405
viet247 <viet24799@gmail.com>
2026-08-13 13:10:50 +0700

    feat: approve transaction

---
+++ Data/Repositories/TransactionRepository.cs
@@ -0,0 +1,24 @@
+﻿
+using FPTRewardSystem.API.Models;
+
+namespace FPTRewardSystem.API.Data.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly AppDbContext _context;
+        public TransactionRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Transaction?> GetByIdAsync(Guid id)
+        {
+            return await _context.Transactions.FindAsync(id);
+        }
+
+        public Task<IEnumerable<Transaction>> GetReconciliationTransactionsAsync(Guid merchantId, DateTime fromDate, DateTime toDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/TransactionStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace FPTRewardSystem.API.Models
+{
+    public enum TransactionStatus
+    {
+        Pending = 0,
+        Approved = 1,
+        Rejected = 2
+    }
+}
